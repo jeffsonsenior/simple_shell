32a25e9cab5536fc8e8533ad7b4c937246cf7375
jeffsonsenior <kinyuajeff@gmail.com>
2023-12-10 07:08:00 -0800

    header filles

---
+++ myshell.h
@@ -0,0 +1,37 @@
+#ifndef MYSHELL_H
+#define MYSHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
